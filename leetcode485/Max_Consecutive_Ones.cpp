df154c135673bbd72836182cf32c93e2e4ca75bd
Paramveer Singh <paramveersinghpanesar28@gmail.com>
2026-01-03 17:52:11 +0530

    Added LeetCode solution: Max Consecutive Ones

---
+++ leetcode485/Max_Consecutive_Ones.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int size=nums.size();
+        int max_count=0;
+        int count=0;
+        for(int i=0;i<size;i++){
+            if(nums[i]==1) count++;
+            else count=0;
+
+            if(count>max_count){
+                max_count=count;
+            }
+        }
+        return max_count;
+        
+    }
+};
